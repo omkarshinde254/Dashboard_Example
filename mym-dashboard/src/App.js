bb65684569ae0c20fe41dbcc9e48905185a86b36
omkarshinde254 <omkarshinde254@gmail.com>
2023-04-10 01:52:30 -0400

    Added tailwind and basic layout

--- mym-dashboard/src/App.js
+++ mym-dashboard/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/Group_185.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. !!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="grid grid-cols-12 h-screen">
+      <div class="col-span-2 bg-[#131313]">
+        <div class="flex flex-row justify-center mt-10">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+      <div class="col-span-4 bg-[#F8F8F8]"></div>
+      <div class="col-span-6"></div>
     </div>
+
   );
 }
 
